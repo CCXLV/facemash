cf59af6c24f0dc4d1bfa66c4a369934c72649371
Mereba11 <abracadabragio12@gmail.com>
2023-09-21 09:30:41 +0400

    Update schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `images` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `unique_id` text COLLATE utf8mb4_unicode_ci,
-    `data` longblob,
-    `name` text COLLATE utf8mb4_unicode_ci,
-    `rating` bigint DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+CREATE TABLE images (
+    id INT NOT NULL AUTO_INCREMENT,
+    unique_id TEXT,
+    data LONGBLOB,
+    name TEXT,
+    rating BIGINT DEFAULT 1400,
+    PRIMARY KEY (id)
+)
